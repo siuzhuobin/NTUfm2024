8f297c1cd193707f2534a59def60f107e780309b
siuzhuobin <125171464+siuzhuobin@users.noreply.github.com>
2024-09-28 03:27:06 +0000

    added faq

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request
+import google.generativeai as genai
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+genai.configure(api_key="AIzaSyDQvy9nXbikgMd890MAyt-penTiMoM7Vek")
+
+
 
 app = Flask(__name__) # erm apparently this is some signing thingy, e.g. for  apps to access like video camera or whatever 
 
@@ -10,13 +16,21 @@ def index():
 def prediction_DBS():
     return(render_template("prediction_DBS.html"))
 
-@app.route("/prediction_result_DBS", methods=["GET","POST"])  
-def prediction_result_DBS():
-    q = float(request.form.get("q"))
-    r0 = -50.6*q+90.2
-    r =f"{r0:.2f}"
-    return(render_template("prediction_result_DBS.html",r=r))
+@app.route("/q1", methods=["GET","POST"])  
+def q1():
+    r = model.generate_content("How should i diversify my investment portfolio?")
+    return(render_template("q1_reply.html",r=r.text))
 
+@app.route("/q2", methods=["GET","POST"])  
+def q2():
+    q = request.form.get('q')
+    r = model.generate_content(q)    
+    return(render_template("q2_reply.html",r=r.text))
+
+
+@app.route("/faq", methods=["GET","POST"])  
+def faq():
+    return(render_template("faq.html"))
 
 if __name__ == "__main__": # apparently, this checks if the thingy is being run onto the cloud? 
     app.run()
